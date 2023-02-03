adabb1a4845186adab14c790d577e8d34e1b9827
eddy_huaylla47@hotmail.com <ehuaylla@attachmedia.com>
2023-02-03 10:59:06 -0500

    Instalando y configurando React Navigation

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { LoginForm } from './src/components/LoginForm';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello World - Eddy</Text>
-      <LoginForm />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Hello World - Eddy</Text>
+        <LoginForm />
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
