e718515e01f7adc3c0e7009883bb071c531d964a
eddy_huaylla47@hotmail.com <ehuaylla@attachmedia.com>
2023-02-03 10:27:50 -0500

    Validación de propiedades: propTypes

--- src/components/Greed/index.js
+++ src/components/Greed/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Text } from "react-native";
+import PropTypes from "prop-types";
+
 
 export const Greed = ( props ) => {
-  const { firstName, lastName } = props;
+  const { firstName, lastName, live } = props;
 
-  return <Text>Hola, { firstName } {lastName } </Text>
+  return <Text>Hola, { firstName } {lastName }. Vives en { live } </Text>
 }
 
 Greed.defaultProps = {
   firstName : 'Eddy',
   lastName : 'Huaylla Quispe'
+}
+
+Greed.propTypes = {
+  firstName : PropTypes.string.isRequired,
+  lastName : PropTypes.string,
+  live : PropTypes.string.isRequired,
 }
--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -5,10 +5,10 @@ import { Greed } from "../Greed";
 export const LoginForm = () => {
 	return (
 		<View>
-			<TextInput placeholder="Email" />
-			<TextInput placeholder="Password" />
-			<Button title="Enviar" onPress={ (e) => console.log( e ) } />
-      <Greed firstName = "Jhon" />
+			<TextInput placeholder = "Email" />
+			<TextInput placeholder = "Password" />
+			<Button title = "Enviar" onPress = { ( e ) => console.log( e ) } />
+      <Greed firstName = "Jhon" live = "Apurimac - Perú" />
 		</View>
 	)
 }
