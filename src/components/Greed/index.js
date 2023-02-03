abf6d283c28d743710016a3803eabd184e8d77d7
eddy_huaylla47@hotmail.com <ehuaylla@attachmedia.com>
2023-02-03 10:09:00 -0500

    Propiedades entre componentes: props

---
+++ src/components/Greed/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+export const Greed = ( props ) => {
+  const { name } = props;
+
+  return <Text>Hola, { name } </Text>
+}
--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, TextInput, View } from "react-native";
+import { Greed } from "../Greed";
 
 export const LoginForm = () => {
 	return (
@@ -7,6 +8,7 @@ export const LoginForm = () => {
 			<TextInput placeholder="Email" />
 			<TextInput placeholder="Password" />
 			<Button title="Enviar" onPress={ (e) => console.log( e ) } />
+      <Greed name="Eddy Huaylla" />
 		</View>
 	)
 }
